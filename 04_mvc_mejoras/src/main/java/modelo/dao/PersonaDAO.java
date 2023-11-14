e82119bae3309f3436b66ffbfe942ebb4b5ec773
Carlos Iñiguez <carlos.iniguez79@gmail.com>
2023-11-14 09:47:40 -0500

    Limpieza de directorio

--- 04_mvc_mejoras/src/main/java/modelo/dao/PersonaDAO.java
+++
@@ -1,7 +0,0 @@
-package modelo.dao;
-
-import modelo.entidades.Persona;
-
-public interface PersonaDAO extends GenericDAO<Persona, Integer>{
-	public Persona autorizar(String nombre, String clave);
-}
