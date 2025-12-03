dd99e3e08092fa8c5261e583402ce55f439650cd
Carlos Iñiguez <carlos.iniguez79@gmail.com>
2025-12-03 14:04:54 -0500

    cambio estilos

---
+++ 06_js/4_cambio_contenido/cambiostyle.js
@@ -0,0 +1,9 @@
+var div = document.getElementsByClassName('ejemplostyle')[1];
+
+// Con set Atribute
+// div.setAttribute('style','height:100px; width:100px; border:2px solid red');
+
+//Con propiedad Style y CSS en CamelCase 
+
+div.style.borderRadius = '50%';
+div.style.border='2px solid red';
