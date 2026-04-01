71fafad8ebbe9c985d8fe179fbaa95035e006d55
Carlos Iñiguez <carlos.iniguez79@gmail.com>
2026-03-31 23:27:30 -0500

    init

--- 07_backend/Test/src/main/java/com/modelo/dao/BancoDAO.java
+++
@@ -1,7 +0,0 @@
-package com.modelo.dao;
-
-import com.modelo.entidades.Banco;
-
-public interface BancoDAO extends GenericDAO<Banco, Integer> {
-
-}
--- 07_backend/Test/src/main/java/com/modelo/dao/CuentaBancariaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.modelo.dao;
-
-import java.util.List;
-
-import com.modelo.entidades.CuentaBancaria;
-
-public interface CuentaBancariaDAO extends GenericDAO<CuentaBancaria, Integer> {
-	public List<CuentaBancaria> listarCuentas();
-}
