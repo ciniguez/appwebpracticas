cfd766dc7c4781c32726906c55f09b5b381efbea
Carlos Iñiguez <carlos.iniguez79@gmail.com>
2026-06-22 15:43:16 -0500

    creacion contenido

--- 04_js/6_script.js
+++ 04_js/6_script.js
@@ -32,3 +32,27 @@ function eliminacionDinamica() {
 
 
 }
+function cargarFichero(){
+
+    var htr = new XMLHttpRequest();
+    htr.open("GET","ficheroJSON.json", true);
+    htr.responseType = "json";
+    htr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log(this.response);
+
+            var elementoUL = document.createElement("ul");
+
+            this.response.list.forEach( elemento => {
+                var li = document.createElement("li");
+                var texto =document.createTextNode(elemento.item);
+
+                li.appendChild(texto);
+                elementoUL.appendChild(li);
+            });
+
+            document.getElementById("datos").appendChild(elementoUL);
+        }
+    }
+    htr.send();
+}
